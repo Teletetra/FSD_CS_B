15f5a72a0ba9cfe88ccbeaa944f2d69f83a07aea
Gaurav kumar <kanishkraval1@gmail.com>
2024-11-08 10:04:21 +0530

    test

---
+++ dom/dom_js.js
@@ -0,0 +1,17 @@
+const para=document.createElement("div");
+para.id="div-1";
+para.style.backgroundColor="red"
+para.style.width="100px";
+document.body.appendChild(para);
+
+const btn1=document.createElement("b ")
+
+// para.innerText="this is my first paragraph"
+
+// const para=document.createElement("p");
+// para.id="para-3";
+// para.style.color="red"
+// para.innerText="this is my first paragraph"
+
+// document.body.appendChild(para);
+
